a84c49203397cb6e0d1eaaef3d6d5b38faa77d65
Laith <62894505+lth17elm@users.noreply.github.com>
2021-03-24 10:08:44 +0100

    progress

--- mysql_to_redis.py
+++ mysql_to_redis.py
@@ -6,7 +6,6 @@ import redis
 import MySQLdb
 from collections import Counter
 
-
 # Mysql server data
 MYSQL_IP_ADDRESS_SERVER = 'localhost'
 MYSQL_USER = 'root'
@@ -18,7 +17,7 @@ REDIS_SERVER = 'localhost'
 
 # function to get data from mysql and to transfer it to redis
 def sql_to_redis():
-    r_redis = redis.StrictRedis(REDIS_SERVER)
+    r = redis.StrictRedis(REDIS_SERVER)
     print ("")
     print ("Connected to Redis successfully!")
 
@@ -33,39 +32,61 @@ def sql_to_redis():
 
     # Clean redis before run again
     # This is for test purpose
-    r_redis.delete("all_records")
+    r.delete("all_records")
 
     # Put all data from MySQL to Redis
     for row in data:
-        r_redis.rpush("all_records", row[3])
+
+        # print(row)
+        # print()
+
+        if( r.dbsize() == 0 ):
+            r.hset( 1, "id", row[0])
+            r.hset( 1, "event-type", row[1])
+            r.hset( 1, "occurredOn", row[2])
+            r.hset( 1, "version", row[3])
+            r.hset( 1, "graph-id", row[4])
+            r.hset( 1, "nature", row[5])
+            r.hset( 1, "object-name", row[6])
+            r.hset( 1, "path", row[7])
+
+            r.set("datas",1)
+        else:              
+            datas = int(r.get("datas").decode()) + 1
+            r.hset( datas, "id", row[0])
+            r.hset( datas, "event-type", row[1])
+            r.hset( datas, "occurredOn", row[2])
+            r.hset( datas, "version", row[3])
+            r.hset( datas, "graph-id", row[4])
+            r.hset( datas, "nature", row[5])
+            r.hset( datas, "object-name", row[6])
+            r.hset( datas, "path", row[7])  
+
+            r.incr("datas")
 
     # Close connection to DB and Cursor
     cursor.close()
     database.close()
 
 
+
 def get_data_from_redis():
+    r2 = redis.StrictRedis(REDIS_SERVER)
+    # LIRE REDIS POUR VERIFIER QUE TOUT EST CORRECTEMENT IMPLEMENTE
 
-    r2_redis = redis.StrictRedis(REDIS_SERVER)
 
-    list = []
-    list = r2_redis.lrange("all_records", 0, 100)
 
-    print (list)
-    print ("")
+def convertToBin(path):
+    res=''
+    pathlist = ['TO_BE_PURGED', 'PURGED' ,'RECEIVED', 'VERIFIED', 'PROCESSED', 'REJECTED', 'REMEDIED', 'CONSUMED']
+    for elem in pathlist:
+        if elem in path:
+            res+='1'
+        else:
+            res+='0'
+        res +=' '
+    return res
 
-    print ("Size of list:", len(list))
-    print ("")
-
-    word_list = [word for line in list for word in line.split()]
-    print (word_list)
-    print ("")
-
-    words_to_count = (word for word in word_list if word[:1].isupper())
-    top_ten = Counter(words_to_count)
-
-    print ("Top 10 Most popular words:")
-    print (top_ten.most_common(10), "\n")
 
 
 sql_to_redis()
